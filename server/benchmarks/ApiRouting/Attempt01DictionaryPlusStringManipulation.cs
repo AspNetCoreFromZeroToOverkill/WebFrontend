08e53d86e1ecf9caed64334c9547a42778219a0a
João Antunes <joao@antunes.dev>
2019-10-07 20:11:05 +0100

    Develop (#36)
    * Multiple service instances tweaks (#31)
    
    * Episode029 (#33)
    
    * Adjust BFF routes
    
    * Using ProxyKit to proxy requests to backing APIs
    
    * Fix vue devserver to proxy the requests correctly
    
    * Reverse MVC order with ProxyKit
    
    Have MVC first, so if we implement some API route it'll be matched, first, otherwise will continue to ProxyKit
    
    * Cosmetic adjustments
    
    * Extract enforcing user authentication to a IMiddleware implementation
    
    * Episode030 (#35)
    
    * Benchmarking ProxiedApiRouteEndpointLookup
    
    * Adjust folder structure
    
    * Rename benchmark class
    
    * Add benchmark results to source control
    
    * Fix incorrect setting in development settings
    
    * Small tweaks and adding inlining diagnostics

---
+++ server/benchmarks/ApiRouting/Attempt01DictionaryPlusStringManipulation.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace CodingMilitia.PlayBall.WebFrontend.BackForFront.Benchmarks.ProxiedApiRouteEndpointLookup
+{
+    public class Attempt01DictionaryPlusStringManipulation : IProxiedApiRouteEndpointLookup
+    {
+        private readonly Dictionary<string, string> _routeToEndpointMap;
+
+        public Attempt01DictionaryPlusStringManipulation(Dictionary<string, string> routeToEndpointMap)
+        {
+            _routeToEndpointMap = routeToEndpointMap;
+        }
+
+        public bool TryGet(PathString path, out string endpoint)
+        {
+            var pathString = path.Value;
+            var basePathEnd = pathString.Substring(1, pathString.Length - 1).IndexOf('/');
+            var basePath = pathString.Substring(1, basePathEnd > 0 ? basePathEnd : pathString.Length - 1);
+            return _routeToEndpointMap.TryGetValue(basePath, out endpoint);
+        }
+    }
+}
