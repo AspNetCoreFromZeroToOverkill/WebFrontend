b628e6cc9605726c8c5162e5deb3f63832e6cf90
João Antunes <joao@antunes.dev>
2019-10-19 12:28:35 +0100

    Reverting back to the simplest ProxiedApiRouteEndpointLookup solution

--- server/src/CodingMilitia.PlayBall.WebFrontend.BackForFront.Web/ApiRouting/ProxiedApiRouteEndpointLookup.cs
+++ server/src/CodingMilitia.PlayBall.WebFrontend.BackForFront.Web/ApiRouting/ProxiedApiRouteEndpointLookup.cs
@@ -8,72 +8,22 @@ namespace CodingMilitia.PlayBall.WebFrontend.BackForFront.Web.ApiRouting
 {
     public class ProxiedApiRouteEndpointLookup
     {
-        // The int dictionary + complex value strategy can be simplified if we're able to lookup directly with a ReadOnlySpan<char>
-        // Work in progress here -> https://github.com/dotnet/corefx/issues/31942
-
-        private readonly Dictionary<int, Holder[]> _routeMatcher;
+        private readonly Dictionary<string, string> _routeToEndpointMap;
 
         public ProxiedApiRouteEndpointLookup(Dictionary<string, string> routeToEndpointMap)
         {
-            var tempRouteMatcher = new Dictionary<int, List<Holder>>();
-            foreach (var entry in routeToEndpointMap)
-            {
-                var hashCode = entry.Key.GetHashCode();
-                if (tempRouteMatcher.TryGetValue(hashCode, out var route))
-                {
-                    route.Add(new Holder(entry.Key, entry.Value));
-                }
-                else
-                {
-                    tempRouteMatcher.Add(hashCode, new List<Holder> {new Holder(entry.Key, entry.Value)});
-                }
-            }
-
-            _routeMatcher = tempRouteMatcher.ToDictionary(e => e.Key, e => e.Value.ToArray());
+            _routeToEndpointMap = routeToEndpointMap;
         }
 
         public bool TryGet(PathString path, out string endpoint)
         {
-            endpoint = null;
-            var pathSpan = path.Value.AsSpan();
-            var basePathEnd = pathSpan.Slice(1, pathSpan.Length - 1).IndexOf('/');
-            var basePath = pathSpan.Slice(1, basePathEnd > 0 ? basePathEnd : pathSpan.Length - 1);
-
-            // when we upgrade to .NET Core 3.0, we can use string.GetHashCode(basePath)
-            // to get the hashcode directly from the span, which will be much better for allocations
-            if (_routeMatcher.TryGetValue(basePath.ToString().GetHashCode(), out var routes))
-            {
-                endpoint = FindRoute(basePath, routes);
-                return endpoint != null;
-            }
-
-            return false;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static string FindRoute(ReadOnlySpan<char> route, Holder[] routes)
-        {
-            foreach (var currentRoute in routes)
-            {
-                if (route.Equals(currentRoute.Route, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    return currentRoute.Endpoint;
-                }
-            }
-
-            return null;
-        }
-
-        private class Holder
-        {
-            public readonly string Route;
-            public readonly string Endpoint;
-
-            public Holder(string route, string endpoint)
-            {
-                Route = route;
-                Endpoint = endpoint;
-            }
+            // If/when we get to index a string keyed dictionary with a span, improve this code to avoid allocations
+            // Discussion here -> https://github.com/dotnet/corefx/issues/31942
+            
+            var pathString = path.Value;
+            var basePathEnd = pathString.Substring(1, pathString.Length - 1).IndexOf('/');
+            var basePath = pathString.Substring(1, basePathEnd > 0 ? basePathEnd : pathString.Length - 1);
+            return _routeToEndpointMap.TryGetValue(basePath, out endpoint);
         }
     }
 }
